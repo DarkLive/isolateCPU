b5ccc0a3bd097f7fd228337d6c462b4bb238eb96
Ege E. Turan <darklive_@live.com>
2017-06-23 08:54:22 +0300

    Add files via upload

---
+++ Source/Properties/Resources.Designer.cs
@@ -0,0 +1,52 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: isolateCPU.Properties.Resources
+// Assembly: isolateCPU, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EE39AFBC-1B67-4B84-B415-DFA0EEFD39E6
+// Assembly location: C:\Users\Dark\Documents\Visual Studio 2015\Projects\isolateCPU\isolateCPU\bin\Release\isolateCPU.exe
+
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+namespace isolateCPU.Properties
+{
+  [GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+  [DebuggerNonUserCode]
+  [CompilerGenerated]
+  internal class Resources
+  {
+    private static ResourceManager resourceMan;
+    private static CultureInfo resourceCulture;
+
+    [EditorBrowsable(EditorBrowsableState.Advanced)]
+    internal static ResourceManager ResourceManager
+    {
+      get
+      {
+        if (isolateCPU.Properties.Resources.resourceMan == null)
+          isolateCPU.Properties.Resources.resourceMan = new ResourceManager("isolateCPU.Properties.Resources", typeof (isolateCPU.Properties.Resources).Assembly);
+        return isolateCPU.Properties.Resources.resourceMan;
+      }
+    }
+
+    [EditorBrowsable(EditorBrowsableState.Advanced)]
+    internal static CultureInfo Culture
+    {
+      get
+      {
+        return isolateCPU.Properties.Resources.resourceCulture;
+      }
+      set
+      {
+        isolateCPU.Properties.Resources.resourceCulture = value;
+      }
+    }
+
+    internal Resources()
+    {
+    }
+  }
+}
---
+++ Source/Properties/Settings.Designer.cs
@@ -0,0 +1,27 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: isolateCPU.Properties.Settings
+// Assembly: isolateCPU, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EE39AFBC-1B67-4B84-B415-DFA0EEFD39E6
+// Assembly location: C:\Users\Dark\Documents\Visual Studio 2015\Projects\isolateCPU\isolateCPU\bin\Release\isolateCPU.exe
+
+using System.CodeDom.Compiler;
+using System.Configuration;
+using System.Runtime.CompilerServices;
+
+namespace isolateCPU.Properties
+{
+  [CompilerGenerated]
+  [GeneratedCode("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
+  internal sealed class Settings : ApplicationSettingsBase
+  {
+    private static Settings defaultInstance = (Settings) SettingsBase.Synchronized((SettingsBase) new Settings());
+
+    public static Settings Default
+    {
+      get
+      {
+        return Settings.defaultInstance;
+      }
+    }
+  }
+}
